cc78f66f229101addcb216d12f63743c1aa22a31
macgarcia <marquim.iron@gmail.com>
2020-01-23 01:53:44 -0300

    00-inicio do projeto

---
+++ src/main/java/com/macgarcia/gpweb/GpWebApplication.java
@@ -0,0 +1,13 @@
+package com.macgarcia.gpweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macgarcia/gpweb/GpWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macgarcia.gpweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
